dd8878cc6cc4f11196e9b1a5d28ec01fa0e91ca3
Saketh Mynampati <sakethmynampati@billericak12.com>
2020-09-25 13:44:14 -0400

    First

--- src/base/Main.java
+++ src/base/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(MathEx.dayOfWeek(0, 1));
+        System.out.println(MathEx.dayOfWeek(0, 22));
+        System.out.println(MathEx.swap(1775));
 
     }
 }
